7b02b2ba7011f38f8b11f01081bbe228e1a0a132
SamHez <samuelepodoi@gmail.com>
2017-12-03 23:40:42 +0300

    First commit

---
+++ Program.py
@@ -0,0 +1,60 @@
+print("Welcome to a Demo App V1.1.0 $$Made by Sam Epodoi$$")
+Name = input("Enter Name:")
+Color = input("Enter your Favorite Color: ")
+Interest = input("What do you like:  ")
+
+
+print("Hey", Name, "How's your day?" )
+
+answer = input("good or bad: ")
+
+if (answer == "good"):
+	print("Awesome", Name,"Enjoy the rest of it!")
+if (answer == "Bad"):
+	print("Sorry", Name,"Lets hope it will get better!")
+if (answer == "bad"):
+	print("Sorry", Name,"Lets hope it will get better!")
+
+if (answer == "Good"):
+	print("Awesome", Name,"Enjoy the rest of it!")
+if (answer == "GOOD"):
+	print("Awesome", Name,"Enjoy the rest of it!")
+if (answer == "gooD"):
+	print("Awesome", Name,"Enjoy the rest of it!")
+if (answer == "gOod"):
+	print("Awesome", Name,"Enjoy the rest of it!")
+if (answer == "gOOd"):
+	print("Awesome", Name,"Enjoy the rest of it!")
+if (answer == "goOd"):
+	print("Awesome", Name,"Enjoy the rest of it!")
+
+	
+input("**Hit Enter to Continue**: ")
+
+print("Lets see if I still remember your favorite color! **Hit Enter to continue**")
+
+input()
+
+print("I think its",Color,)
+
+Color_input = input("Yes or No: ")
+
+if(Color_input == "Yes"):
+	print("Yeyy!! I got it right.... Im the most intelligent computer-bot!! :)")
+if(Color_input == "yes"):
+	print("Yeyy!! I got it right.... Im the most intelligent computer-bot!! :)")
+if(Color_input == "YES"):
+	print("Yeyy!! I got it right.... Im the most intelligent computer-bot!! :)")
+if(Color_input == "bad"):
+	print("Oh no! This is sad.. I may have to go back to bot school :( ")
+if(Color_input == "Bad"):
+	print("Oh no! This is sad.. I may have to go back to bot school :( ")
+if(Color_input == "BAD"):
+	print("Oh no! This is sad.. I may have to go back to bot school :( ")
+	
+input("**Hit Enter to Continue**: ")
+
+print("It has been awesome interacting with you ",Name, "... By the way I also like ", Interest,)
+input()
+
+	
